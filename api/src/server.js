9107cd9f69b42737fafa9803dee9842b9d878916
Rodrigonovato10 <luisrodrigo.furtado123@gmail.com>
2024-11-04 11:41:59 -0300

    corrigindo strings no servidor

--- api/src/server.js
+++ api/src/server.js
@@ -2,6 +2,5 @@ import app from './app.js';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, '0.0.0.0', () =>
-  console.log(`🛜  Server running at port ${port}...`),
-);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`🛜  Server running at port ${port}...`)});
